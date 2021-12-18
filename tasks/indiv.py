746bb25ed46dd953dcd57062c2bd0adb2beb9e15
HAXF13D <vovik0312@gmail.com>
2021-12-18 14:05:07 +0300

    Add pep8 changes

--- tasks/indiv.py
+++ tasks/indiv.py
@@ -3,12 +3,12 @@
 
 
 def individual_func(string):
-
     def replace_string(name, surname):
         nonlocal string
         answer = string.replace("%N%", name)
         answer = answer.replace("%F%", surname)
         return answer
+
     return replace_string
 
 
